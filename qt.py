6b5f6389d027b5a57f37ef0d32ff26ef6564e3b4
Yonguk Lee <qjrmsktso2@gmail.com>
2019-06-01 22:52:55 +0900

    add input wiget

--- qt.py
+++ qt.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QFileDialog, QMainWindow, QInputDialog
 
 
 class MyApp(QWidget):
@@ -13,13 +13,21 @@ class MyApp(QWidget):
         grid = QGridLayout()
         self.setLayout(grid)
 
-        grid.addWidget(QLabel('Title:'), 0, 0)
-        grid.addWidget(QLabel('Author:'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
+        grid.addWidget(QLabel('채점할 폴더 : '), 0, 0)
+        grid.addWidget(QLabel('학생 정보 엑셀 파일 : '), 1, 0)
+        grid.addWidget(QLabel('학생 수 : '), 2, 0)
+        grid.addWidget(QLabel('입력 1 : '), 3, 0)
+        grid.addWidget(QLabel('입력 2 : '), 4, 0)
+        grid.addWidget(QLabel('입력 3 : '), 5, 0)
+        grid.addWidget(QLabel('입력 3 : '), 6, 0)
 
         grid.addWidget(QLineEdit(), 0, 1)
         grid.addWidget(QLineEdit(), 1, 1)
         grid.addWidget(QTextEdit(), 2, 1)
+        grid.addWidget(QTextEdit(), 3, 1)
+        grid.addWidget(QTextEdit(), 4, 1)
+        grid.addWidget(QTextEdit(), 5, 1)
+        grid.addWidget(QTextEdit(), 6, 1)
 
         self.setWindowTitle('QGridLayout')
         self.setGeometry(300, 300, 300, 200)
@@ -33,6 +41,9 @@ class MyApp(QWidget):
         files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         if files:
             print(files)
+
+    def getTextInput(self):
+        return QInputDialog.getText(self, 'Input Dialog', 'Enter your name:')
 
 if __name__ == '__main__':
 
